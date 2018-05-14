fb06cb6fabaf8ef951f05ae8b5e77b727034a6f8
Austin Ray <austin@austinray.io>
2018-05-14 17:56:14 -0400

    Convert PopupCallback to Java

---
+++ src/main/java/ccpl/numberline/config/PopupCallback.java
@@ -0,0 +1,15 @@
+package ccpl.numberline.config;
+
+import ccpl.lib.Bundle;
+
+public class PopupCallback {
+  Bundle bundle;
+
+  public PopupCallback() {
+    bundle = new Bundle();
+  }
+
+  public Bundle getBundle() {
+    return bundle;
+  }
+}
--- src/main/kotlin/ccpl/numberline/config/PopupCallback.kt
+++
@@ -1,7 +0,0 @@
-package ccpl.numberline.config
-
-import ccpl.lib.Bundle
-
-class PopupCallback {
-  var bundle: Bundle = Bundle()
-}
